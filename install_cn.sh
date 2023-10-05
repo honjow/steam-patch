4437ec1efb3870ea33ac9174bbda8128b2581734
honjow <honjow311@gamil.com>
2023-10-06 05:53:55 +0800

    add install cn

---
+++ install_cn.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+github_prefix=$1
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+echo "Installing Steam Patch release..."
+
+USER_DIR="$(getent passwd $SUDO_USER | cut -d: -f6)"
+WORKING_FOLDER="${USER_DIR}/steam-patch"
+
+# Create folder structure
+mkdir -p "${WORKING_FOLDER}"
+# Enable CEF debugging
+touch "${USER_DIR}/.steam/steam/.cef-enable-remote-debugging"
+
+# Download latest release and install it
+RELEASE=$(curl -s '${github_prefix}https://api.github.com/repos/honjow/steam-patch/releases' | jq -r "first(.[] | select(.prerelease == "false"))")
+VERSION=$(jq -r '.tag_name' <<< ${RELEASE} )
+DOWNLOAD_URL=$(jq -r '.assets[].browser_download_url | select(endswith("steam-patch"))' <<< ${RELEASE})
+
+systemctl --user stop steam-patch 2> /dev/null
+systemctl --user disable steam-patch 2> /dev/null
+
+systemctl stop steam-patch 2> /dev/null
+systemctl disable steam-patch 2> /dev/null
+
+printf "Installing version %s...\n" "${VERSION}"
+curl -L "${github_prefix}${DOWNLOAD_URL}" --output ${WORKING_FOLDER}/steam-patch
+chmod +x ${WORKING_FOLDER}/steam-patch
+
+# Add new service file
+cat > "${WORKING_FOLDER}/steam-patch.service" <<- EOM
+[Unit]
+Description=Steam Patches Loader
+Wants=network.target
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=${WORKING_FOLDER}/steam-patch --user=${SUDO_USER}
+WorkingDirectory=${WORKING_FOLDER}
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+rm -f "/etc/systemd/system/steam-patch.service"
+cp "${WORKING_FOLDER}/steam-patch.service" "/etc/systemd/system/steam-patch.service"
+
+# Run service
+systemctl daemon-reload
+systemctl enable steam-patch.service
+systemctl start steam-patch.service
