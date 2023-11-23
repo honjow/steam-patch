210c436938d68fdf0c4ca71669f090feba52f9de
LtChipotle <diegocorando@gmail.com>
2023-11-22 19:06:45 -0800

    Add a new config item

--- src/config.rs
+++ src/config.rs
@@ -22,6 +22,8 @@ pub struct Config {
     pub legacy_tdp: bool,
     #[serde(default = "default_auto_nkey_recovery")]
     pub auto_nkey_recovery: bool,
+    #[serde(default = "default_spoof_glyphs")]
+    pub spoof_glyphs: bool,
 }
 
 // Default functions for each field
@@ -33,6 +35,7 @@ fn default_max_gpu() -> i16 { 2000 }
 fn default_mapper() -> bool { true }
 fn default_legacy_tdp() -> bool { true }
 fn default_auto_nkey_recovery() -> bool { false }
+fn default_spoof_glyphs() -> bool { true }
 
 
 
@@ -63,6 +66,7 @@ pub fn initialize_config() -> Config {
                 mapper: true,
                 legacy_tdp: true,
                 auto_nkey_recovery: false,
+                spoof_glyphs: true,
             };
             *global_config = Some(default_config.clone());
             default_config
